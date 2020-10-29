9154a99986503763e0b976692b0500b44af5ea24
Georgescu Rares Daniel <65974277+RaresGeo@users.noreply.github.com>
2020-10-29 15:54:14 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,37 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+
+substrings = input(
+    'Enter substring to search for. Separate with a single comma for multiple words: ')
+substrings = substrings.split(",")
+
+output = open('quotes.txt', 'w')
+
+for word in substrings:
+    start = 'https://practicalguidetoevil.wordpress.com/2015/04/01/chapter-1-knife/'
+    output.write('Instances of the word "' + word +
+                 '" were found in the following chapters: \n\n')
+    count = 0
+    length = 0
+    while True:
+        source = requests.get(start).text
+
+        soup = BeautifulSoup(source, 'lxml')
+
+        text_ = soup.find('div', class_='entry-wrapper')
+        if text_:
+            length = len(re.findall(word, text_.text, re.IGNORECASE))
+            count += length
+            if length > 0:
+                output.write(start + ' - ' + str(length) + ' instances\n')
+            print(str(count) + "\n")
+
+        match = soup.find('div', class_='nav-next')
+
+        if match:
+            match = match.a
+            start = match['href']
+        else:
+            output.write('\n')
+            break
